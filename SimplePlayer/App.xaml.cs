d9b6210064f710834511af730779145ff19fb263
4egod <dmitry.tarasov@live.com>
2020-09-03 09:06:15 +0500

    feat(SimplePlayer): add debug/trace listeners to view debug output

--- SimplePlayer/App.xaml.cs
+++ SimplePlayer/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,11 @@ namespace SimplePlayer
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+        }
     }
 }
